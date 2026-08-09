7a84f3e0231f7c23cd7ee112e4c943609bd96c28
Yahav <hvcvdr@gmail.com>
2026-08-09 14:30:35 +0300

    HG-219-Added-Screens

---
+++ src/wolt-app/app/(tabs)/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function DiscoveryScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Discovery Screen</Text>
+      {/* TODO: Implement Discovery/Home UI */}
+    </View>
+  );
+}
---
+++ src/wolt-app/app/(tabs)/orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function OrdersScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Orders Screen</Text>
+      {/* TODO: Implement Orders UI */}
+    </View>
+  );
+}
